43d6f4bb9f3f0fc62287ed416761fc748c9f028f
tan19uch1 <tongue.of.wolf@gmail.com>
2020-05-09 11:34:31 +0900

    fuga

--- wordwolf/settings.py
+++ wordwolf/settings.py
@@ -15,9 +15,8 @@ import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-#BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-django_heroku.settings(locals())
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
